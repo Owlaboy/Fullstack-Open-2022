9b10e14e9a11ad68f2543653dbd5f928c4482411
Andrei Home <andrei.preoteasa.ap@gmail.com>
2023-02-04 22:22:27 +0200

    updated dataservice

--- part2/phonebook/src/dataService.js
+++ part2/phonebook/src/dataService.js
@@ -1,5 +1,6 @@
 import axios from "axios"
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://part3-1g7g.onrender.com/api/persons'
+// http://localhost:3001/api/persons
 
 const getAll = () => {
   const request = axios.get(baseUrl)
